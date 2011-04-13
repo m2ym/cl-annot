66a57bf7df2775b48b6471ecb36936817d0c17ff
Tomohiro Matsuyama <tomo@cx4a.org>
2011-04-13 13:47:28 +0900

    Fixed class annotations.

--- src/lib/class.lisp
+++ src/lib/class.lisp
@@ -11,42 +11,48 @@
 
 (defannotation metaclass (metaclass class-definition-form)
     (:arity 2)
-  (let ((class-definition-form (progn-form-last class-definition-form)))
-    (if (get-class-option :metaclass class-definition-form)
+  (progn-form-replace-last
+   (lambda (class-definition-form)
+     (if (get-class-option :metaclass class-definition-form)
         (error ":metaclass is not empty")
         (append class-definition-form
-                `((:metaclass ,metaclass))))))
+                `((:metaclass ,metaclass)))))
+   class-definition-form))
 
 (defmacro export-slots (class-definition-form)
-  (let ((class-definition-form (progn-form-last class-definition-form)))
-    (loop for slot-specifier in (slot-specifiers class-definition-form)
-          for slot = (if (consp slot-specifier)
-                         (car slot-specifier)
-                         slot-specifier)
-          collect slot into slots
-          finally
-       (return
-         (if slots
-             `(progn
-                (export ',slots)
-                ,class-definition-form)
-             class-definition-form)))))
+  (progn-form-replace-last
+   (lambda (class-definition-form)
+     (loop for slot-specifier in (slot-specifiers class-definition-form)
+           for slot = (if (consp slot-specifier)
+                          (car slot-specifier)
+                          slot-specifier)
+           collect slot into slots
+           finally
+        (return
+          (if slots
+              `(progn
+                 (export ',slots)
+                 ,class-definition-form)
+              class-definition-form))))
+   class-definition-form))
 
 (defmacro export-accessors (class-definition-form)
-  (let ((class-definition-form (progn-form-last class-definition-form)))
-    (loop for slot-specifier in (slot-specifiers class-definition-form)
-          for slot-options = (when (consp slot-specifier) (cdr slot-specifier))
-          if slot-options
-            append (plist-get-all slot-options :reader) into accessors
-            and append (plist-get-all slot-options :writer) into accessors
-            and append (plist-get-all slot-options :accessor) into accessors
-          finally
-       (return
-         (if accessors
-             `(progn
-                (export ',accessors)
-                ,class-definition-form)
-             class-definition-form)))))
+  (progn-form-replace-last
+   (lambda (class-definition-form)
+     (loop for slot-specifier in (slot-specifiers class-definition-form)
+           for slot-options = (when (consp slot-specifier) (cdr slot-specifier))
+           if slot-options
+             append (plist-get-all slot-options :reader) into accessors
+             and append (plist-get-all slot-options :writer) into accessors
+             and append (plist-get-all slot-options :accessor) into accessors
+           finally
+        (return
+          (if accessors
+              `(progn
+                 (export ',accessors)
+                 ,class-definition-form)
+              class-definition-form))))
+   class-definition-form))
 
 (defmacro export-class (class-definition-form)
   `(annot.std:export*
